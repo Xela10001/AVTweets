fc80374c70bc2d5ca9b929226b80add64fb4b17f
Xela10001 <alexandre.gazur@ua.pt>
2023-01-26 10:45:40 +0000

    Added color coding to graphs

--- graphs.py
+++ graphs.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+colorCodes = {
+	'brasil': 'limegreen',
+	'religiao': 'yellow',
+	'ataque': 'red',
+	'shitpost': 'darkgoldenrod',
+	'racismo': 'black',
+	'corrupcao': 'indianred',
+	'defesa': 'deepskyblue',
+	'racismo leve': 'slategray',
+	'anti-lgbt': 'pink',
+	'pro-policia': 'darkblue'
+}
+
 def getTweetContents(tweet):
 	split = tweet.split(',')
 	tweetID = split[0]
@@ -22,7 +35,7 @@ def turnDictionaryIntoSortedLists(categoryNumbers:dict):
 	list1 = sorted([(a, b) for a, b in categoryNumbers.items()], key=lambda x: x[1][0]/x[1][1], reverse=True)
 	keys = [a + "\n" + str(b[1]) for a, b in list1]
 	values = [b[0]/b[1] for a, b in list1]
-	return keys, values
+	return keys[:8], values[:8]
 def getLikeAverageByCategory(tweets):
 	categoryNumbers = {}
 	for tweet in tweets:
@@ -41,8 +54,18 @@ def getLikeAverageByCategory(tweets):
 def createBarPlot(categoryAverage:tuple):
 	keys, values = categoryAverage
 
+	colors = [colorCodes[a.split('\n')[0]] for a in keys]
+
 	# creating the bar plot
-	plt.bar(keys, values, color='maroon',
+	plt.bar(keys[:-1], values[:-1], color=colors[:-1],
+			width=0.4)
+
+	plt.xlabel("Categorias")
+	plt.ylabel("Likes")
+	plt.title("Média de likes de tweets do André Ventura por categoria")
+	plt.show()
+
+	plt.bar(keys[1:], values[1:], color=colors[1:],
 			width=0.4)
 
 	plt.xlabel("Categorias")
