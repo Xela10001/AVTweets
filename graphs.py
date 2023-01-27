962530b521fa5b9a0003b6aec025634463fb87b7
Xela10001 <alexandre.gazur@ua.pt>
2023-01-27 00:39:55 +0000

    Created more graphs, simplified their code, made them better.

--- graphs.py
+++ graphs.py
@@ -30,13 +30,7 @@ def getTweetsFromCSV(file):
 	for line in file:
 		tweets.append(getTweetContents(line))
 	return tweets
-
-def turnDictionaryIntoSortedLists(categoryNumbers:dict):
-	list1 = sorted([(a, b) for a, b in categoryNumbers.items()], key=lambda x: x[1][0]/x[1][1], reverse=True)
-	keys = [a + "\n" + str(b[1]) for a, b in list1]
-	values = [b[0]/b[1] for a, b in list1]
-	return keys[:8], values[:8]
-def getLikeAverageByCategory(tweets):
+def getNumberOfTweetsAndTotalLikesByCategory(tweets):
 	categoryNumbers = {}
 	for tweet in tweets:
 		likes = tweet[2]
@@ -49,31 +43,61 @@ def getLikeAverageByCategory(tweets):
 				previousLikes, previousNumber = categoryNumbers[category]
 				categoryNumbers[category] = (previousLikes + likes, previousNumber + 1)
 
-	return turnDictionaryIntoSortedLists(categoryNumbers)
+	return categoryNumbers
+def turnDictionaryIntoSortedAverageLists(categoryNumbers:dict):
+	list1 = sorted([(a, b) for a, b in categoryNumbers.items()], key=lambda x: x[1][0]/x[1][1], reverse=True)
+	keys = [a + "\n" + str(b[1]) for a, b in list1]
+	values = [b[0]/b[1] for a, b in list1]
+	return keys, values
+def getLikeAverageByCategory(tweets):
+	sortedTweets = getNumberOfTweetsAndTotalLikesByCategory(tweets)
+	return turnDictionaryIntoSortedAverageLists(sortedTweets)
+def turnDictionaryIntoSortedTotalTweetsList(categoryNumbers:dict):
+	list1 = sorted([(a, b) for a, b in categoryNumbers.items()], key=lambda x: x[1][1], reverse=True)
+	keys = [a + "\n" + str(b[1]) for a, b in list1]
+	values = [b[0] / b[1] for a, b in list1]
+	return keys, values
+def getMostTweetsByCategory(tweets):
+	sortedTweets = getNumberOfTweetsAndTotalLikesByCategory(tweets)
+	return turnDictionaryIntoSortedTotalTweetsList(sortedTweets)
+def findBrasil(keys):
+	for i, name in enumerate(keys):
+		if "brasil" in name:
+			return i
+def showGraph(keys, values, colors, title, yLim=-1):
+	f = plt.figure()
+	f.set_figwidth(6)
+	f.set_figheight(6)
+	plt.bar(keys, values, color=colors, width=0.7)
+	if yLim != -1:
+		plt.ylim(0, yLim)
+	plt.xlabel("Categorias")
+	plt.ylabel("Likes")
+	plt.title(title)
+	plt.show()
 
-def createBarPlot(categoryAverage:tuple):
+def createBarPlot(categoryAverage:tuple, categoryTotal:tuple):
 	keys, values = categoryAverage
+	colors = [colorCodes.setdefault(a.split('\n')[0], 'maroon') for a in keys]
 
-	colors = [colorCodes[a.split('\n')[0]] for a in keys]
+	showGraph(keys[:7], values[:7], colors[:7], "Média de likes de tweets do André Ventura\nOrdenado por categoria (Com Brasil)\n")
 
-	# creating the bar plot
-	plt.bar(keys[:-1], values[:-1], color=colors[:-1],
-			width=0.4)
+	i = findBrasil(keys)
+	keys.pop(i)
+	values.pop(i)
+	colors.pop(i)
+	showGraph(keys[:6], values[:6], colors[:6], "Média de likes de tweets do André Ventura\nOrdenado por categoria (Sem Brasil)\n1º Parte", yLim=1500)
+	showGraph(keys[6:], values[6:], colors[6:], "Média de likes de tweets do André Ventura\nOrdenado por categoria (Sem Brasil)\n2º Parte", yLim=1500)
 
-	plt.xlabel("Categorias")
-	plt.ylabel("Likes")
-	plt.title("Média de likes de tweets do André Ventura por categoria")
-	plt.show()
-
-	plt.bar(keys[1:], values[1:], color=colors[1:],
-			width=0.4)
-
-	plt.xlabel("Categorias")
-	plt.ylabel("Likes")
-	plt.title("Média de likes de tweets do André Ventura por categoria")
-	plt.show()
+	keys, values = categoryTotal
+	i = findBrasil(keys)
+	keys.pop(i)
+	values.pop(i)
+	colors = [colorCodes.setdefault(a.split('\n')[0], 'maroon') for a in keys]
+	showGraph(keys, values, colors, "Tweets do André Ventura\nOrdenados por nº total de tweets", yLim=1500)
 
 
 tweets = getTweetsFromCSV("tweets.txt")
 categoryAverage = getLikeAverageByCategory(tweets)
-createBarPlot(categoryAverage)
+categoryTotal = getMostTweetsByCategory(tweets)
+createBarPlot(categoryAverage, categoryTotal)
